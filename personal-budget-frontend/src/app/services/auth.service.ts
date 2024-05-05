c6980280531b47359ae0115a7671549d583f037e
Alish Jain <alishjain@Alishs-MBP.attlocal.net>
2024-05-05 17:10:07 -0400

    changes

--- personal-budget-frontend/src/app/services/auth.service.ts
+++ personal-budget-frontend/src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class AuthService {
   private isAuthenticatedSubject: BehaviorSubject<boolean>;
-  private apiUrl = 'http://18.216.195.177:3000';
+  private apiUrl = 'http://3.17.23.84:3000';
   private dialogRefer: MatDialogRef<TokenExpiryComponent> | undefined;
 
   constructor(
