151b70c4e93bad0a63c5a98d81b2eafddd63a24e
Alish Jain <alishjain@Alishs-MBP.attlocal.net>
2024-05-06 18:03:04 -0400

    changs

--- personal-budget-frontend/server.ts
+++ personal-budget-frontend/server.ts
@@ -6,11 +6,11 @@ const server = require('http').Server(app);
 
 
 // Serve only the static files form the angular app directory
-app.use(express.static(__dirname + '/your-application-name'));
+app.use(express.static(__dirname + '/personal-budget-frontend/personal-budget-frontend'));
 
 app.get('/*', function(req: any, res: { sendFile: (arg0: any) => void; }) {
 
-res.sendFile(path.join(__dirname+'/your-application-name/index.html'));
+res.sendFile(path.join(__dirname+'/personal-budget-frontend/personal-budget-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
