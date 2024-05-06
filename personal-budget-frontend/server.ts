33879e0a6f5092a236026dab902335bdf99d520e
Alish Jain <alishjain@Alishs-MBP.attlocal.net>
2024-05-06 18:04:39 -0400

    changs

--- personal-budget-frontend/server.ts
+++ personal-budget-frontend/server.ts
@@ -1,21 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env['PORT'] || 3000;
-const server = require('http').Server(app);
 
+const app = express();
 
-// Serve only the static files form the angular app directory
-app.use(express.static(__dirname + '/personal-budget-frontend/personal-budget-frontend'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/personal-budget-frontend'));
 
-app.get('/*', function(req: any, res: { sendFile: (arg0: any) => void; }) {
+app.get('/*', function(req: any,res: { sendFile: (arg0: any) => void; }) {
 
-res.sendFile(path.join(__dirname+'/personal-budget-frontend/personal-budget-frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/personal-budget-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-
-
-server.listen(port, function () {
-    console.log("App running on port " + port);
-})
+app.listen(process.env['PORT'] || 8080);
